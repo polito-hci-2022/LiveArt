b2ef9e91393dc05b2d808e78d452b75d99155867
Edoardo Colella <edoardocolella.it@gmail.com>
2023-01-12 19:43:44 +0100

    reworked search

--- LiveArt_Unity/Assets/Scripts/SearchScript.cs
+++ LiveArt_Unity/Assets/Scripts/SearchScript.cs
@@ -5,77 +5,49 @@ using UnityEngine.UI;
 public class SearchScript : MonoBehaviour
 {
     public GameObject ContentHolder;
-
     public GameObject ErrorSearchMessage;
-
     GameObject[] Element;
-
     public InputField SearchBar;
-
-    int totalElements;
-
-    int temp;
 
     // Start is called before the first frame update
     void Start()
     {
-        totalElements = ContentHolder.transform.childCount;
-
+        int totalElements = ContentHolder.transform.childCount - 1;
         Element = new GameObject[totalElements];
 
         for (int i = 0; i < totalElements; i++)
-        {
-            Element[i] = ContentHolder.transform.GetChild(i).gameObject;
-            if (Element[i].name == ErrorSearchMessage.name)
-                Element[i].SetActive(false);
-        }
+            if (ContentHolder.transform.GetChild(i + 1).gameObject.name != ErrorSearchMessage.name)
+                Element[i] = ContentHolder.transform.GetChild(i + 1).gameObject;
     }
 
     // Update is called once per frame
     public void Search()
     {
-        string SearchText = SearchBar.text;
-        int searchLength = SearchText.Length;
-
         int activElements = 0;
 
-        if (searchLength > 0)
+        if (SearchBar.text.Length > 0)
         {
             foreach (GameObject ele in Element)
             {
                 if (
-                    ele.transform.GetChild(0).GetComponent<TMP_Text>().text.Length >= searchLength
-                    && SearchText.ToLower()
-                        == ele.transform
-                            .GetChild(0)
-                            .GetComponent<TMP_Text>()
-                            .text.Substring(0, searchLength)
-                            .ToLower()
+                    ele.transform
+                        .GetChild(0)
+                        .GetComponent<TMP_Text>()
+                        .text.ToLower()
+                        .Contains(SearchBar.text.ToLower())
                 )
-                {
                     activElements += 1;
-                }
                 else
                     ele.SetActive(false);
             }
+            if (activElements == 0)
+                ErrorSearchMessage.SetActive(true);
         }
         else
         {
-            activElements = 1;
-            temp = 0;
             foreach (GameObject ele in Element)
-            {
-                if (ele.name != ErrorSearchMessage.name)
-                {
-                    ele.SetActive(true);
-                    temp++;
-                }
-                else
-                    ele.SetActive(false);
-            }
-            activElements = temp;
+                ele.SetActive(true);
+            ErrorSearchMessage.SetActive(false);
         }
-        if (activElements == 0)
-            ErrorSearchMessage.SetActive(true);
     }
 }
