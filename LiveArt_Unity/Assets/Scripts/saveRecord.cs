52f0992993eb024ca1ada6df8e4808e486ccb77d
Edoardo Colella <edoardocolella.it@gmail.com>
2023-01-12 19:16:00 +0100

    Attempts

--- LiveArt_Unity/Assets/Scripts/saveRecord.cs
+++ LiveArt_Unity/Assets/Scripts/saveRecord.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -16,17 +17,44 @@ public class saveRecord : MonoBehaviour
     public InputField DescripitonInput;
 
     public GameObject thankText;
+    string[] lines;
+
+    [Serializable]
+    public class WOA
+    {
+        public string title,
+            author,
+            description;
+
+        public WOA(string nTitle, string nAuthor, string nDescription)
+        {
+            title = nTitle;
+            author = nAuthor;
+            description = nDescription;
+        }
+
+        public void print()
+        {
+            Debug.Log("Debug: " + title + " " + author + " " + description);
+        }
+    }
+
+    void Start()
+    {
+        readData();
+    }
 
     public void AggiungiWoA()
     {
-        InstaziaNuovoRecord(TitleInput.text, AuthorInput.text, DescripitonInput.text);
+        InstaziaNuovoRecord(TitleInput.text, AuthorInput.text, DescripitonInput.text, true);
+        writeData(TitleInput.text, AuthorInput.text, DescripitonInput.text);
         TitleInput.text = "";
         AuthorInput.text = "";
         DescripitonInput.text = "";
         thankText.SetActive(true);
     }
 
-    void InstaziaNuovoRecord(string title, string author, string description)
+    void InstaziaNuovoRecord(string title, string author, string description, bool flag)
     {
         GameObject element = (GameObject)Instantiate(prefabContent);
         element.transform.GetChild(0).gameObject.GetComponent<TMP_Text>().SetText(title);
@@ -35,9 +63,30 @@ public class saveRecord : MonoBehaviour
         element.transform.SetParent(parent.transform);
         element.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
         element.GetComponent<RectTransform>().localRotation = Quaternion.Euler(0, 0, 0);
-        element.GetComponent<RectTransform>().localPosition = new Vector3(170, posY, 0);
+        if (flag)
+            element.GetComponent<RectTransform>().localPosition = new Vector3(170, posY, 0);
+        else
+            element.GetComponent<RectTransform>().localPosition = new Vector3(0, posY, 0);
         posY = posY - 45;
-
     }
 
+    void readData()
+    {
+        lines = System.IO.File.ReadAllLines(
+            @"C:\Users\edoar\code\LiveArt\LiveArt_Unity\Assets\DATA.txt"
+        );
+        WOA woa;
+        foreach (string str in lines)
+        {
+            woa = JsonUtility.FromJson<WOA>(str);
+            InstaziaNuovoRecord(woa.title, woa.author, woa.description, false);
+        }
+    }
+
+    async void writeData(string title, string author, string description)
+    {
+        using StreamWriter file =
+            new(@"C:\Users\edoar\code\LiveArt\LiveArt_Unity\Assets\DATA.txt", append: true);
+        await file.WriteLineAsync(JsonUtility.ToJson(new WOA(title, author, description)));
+    }
 }
