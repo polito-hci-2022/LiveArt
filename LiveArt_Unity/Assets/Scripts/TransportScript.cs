79a1a9cfbfacf63764dbf7fe907f01ada5b0cf48
Edoardo Colella <edoardocolella.it@gmail.com>
2023-01-12 14:02:36 +0100

    skeleton of scene and clear button

--- LiveArt_Unity/Assets/Scripts/TransportScript.cs
+++ LiveArt_Unity/Assets/Scripts/TransportScript.cs
@@ -30,10 +30,28 @@ public class TransportScript : MonoBehaviour
             StartCoroutine(TeleportTo(0, 250, 0, "room1_1"));
     }
 
+    public void TeleportRoom1_2()
+    {
+        if (!transition && mode != "room1_2")
+            StartCoroutine(TeleportTo(0, 500, 0, "room1_2"));
+    }
+
     public void TeleportRoom2()
     {
         if (!transition && mode != "room2")
             StartCoroutine(TeleportTo(250, -22, 0, "room2"));
+    }
+
+    public void TeleportRoom2_1()
+    {
+        if (!transition && mode != "room2_1")
+            StartCoroutine(TeleportTo(250, 250, 0, "room2_1"));
+    }
+
+    public void TeleportRoom2_2()
+    {
+        if (!transition && mode != "room2_2")
+            StartCoroutine(TeleportTo(250, 500, 0, "room2_2"));
     }
 
     public void TeleportRoom3()
@@ -42,10 +60,28 @@ public class TransportScript : MonoBehaviour
             StartCoroutine(TeleportTo(500, 0, 0, "room3"));
     }
 
+    public void TeleportRoom3_1()
+    {
+        if (!transition && mode != "room3_1")
+            StartCoroutine(TeleportTo(750, 250, 0, "room3_1"));
+    }
+
+    public void TeleportRoom3_2()
+    {
+        if (!transition && mode != "room3_2")
+            StartCoroutine(TeleportTo(750, 500, 0, "room3_2"));
+    }
+
     public void TeleportRoom4()
     {
         if (!transition && mode != "room4")
             StartCoroutine(TeleportTo(750, 0, 0, "room4"));
+    }
+
+    public void TeleportRoom4_1()
+    {
+        if (!transition && mode != "room4_1")
+            StartCoroutine(TeleportTo(750, 250, 0, "room4_1"));
     }
 
     IEnumerator TeleportTo(int x, int y, int z, string newMode)
